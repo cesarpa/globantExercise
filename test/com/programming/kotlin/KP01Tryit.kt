95e66e310c301b86283247063067beebf2849f4e
Cesarpa Code <cesar.1023@hotmail.com>
2022-11-02 22:16:50 -0500

    K - variables beggining

---
+++ test/com/programming/kotlin/KP01Tryit.kt
@@ -0,0 +1,13 @@
+package com.programming.kotlin
+
+fun main(args: Array<String>){
+    println("Hi I'm Cesarpa")
+
+
+    var variable = "Apple"
+    variable = "Google"
+
+    val name = "Cesar"
+   // name ="" the variable cannot be reassigned
+
+}
